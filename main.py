f69b4d590c848a6ef6d8407f0d3747d8686fd0ed
JonathanSotoP <94851586+JonathanSotoP@users.noreply.github.com>
2026-03-14 18:56:06 -0300

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-from news_fetcher import get_news
-from ai_analyzer import analyze_news
+import feedparser
 from telegram_sender import send_message
+from ai_analyzer import analyze_news
 
-print("Buscando noticias...")
 
-news = get_news()
+RSS_URL = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
 
-print("Noticias encontradas:", len(news))
 
-for article in news:
+def get_news():
 
-    print("Analizando:", article["title"])
+    feed = feedparser.parse(RSS_URL)
 
-    analysis = analyze_news(
-        article["title"],
-        article["summary"]
-    )
+    return feed.entries[:10]
 
-    if analysis:
 
-        message = analysis + f"\n🔗 Fuente:\n{article['link']}"
+def main():
+
+    print("Buscando noticias...")
+
+    news = get_news()
+
+    print("Noticias encontradas:", len(news))
+
+    analyzed = []
+
+    for item in news:
+
+        title = item.title
+        content = item.summary
+
+        print("Analizando:", title)
+
+        score, message = analyze_news(title, content)
+
+        analyzed.append((score, message))
+
+    analyzed.sort(reverse=True)
+
+    top_news = analyzed[:3]
+
+    for _, message in top_news:
 
         send_message(message)
 
-        print("Mensaje enviado")
+
+if __name__ == "__main__":
+    main()
