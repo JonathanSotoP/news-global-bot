ffd70598d65db7d7af1c96f7b3010ea1bc4e29c2
JonathanSotoP <94851586+JonathanSotoP@users.noreply.github.com>
2026-03-14 14:57:51 -0300

    Actualizar telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -3,13 +3,11 @@ from config import BOT_TOKEN, CHAT_ID
 
 bot = Bot(token=BOT_TOKEN)
 
+
 def send_message(msg):
-    try:
-        bot.send_message(
-            chat_id=CHAT_ID,
-            text=msg,
-            parse_mode="Markdown",
-            disable_web_page_preview=False
-        )
-    except Exception as e:
-        print("Error enviando mensaje a Telegram:", e)
+
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text=msg,
+        disable_web_page_preview=False
+    )
